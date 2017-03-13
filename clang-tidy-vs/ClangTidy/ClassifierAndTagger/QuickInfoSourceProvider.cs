e1d90e8671667bac65255258f378d51b0b836f24
jutocz <jutocz@gmail.com>
2017-03-13 16:23:13 +0100

    clang-tidy VS plugin extended
    Extending clang-tidy Visual Studio plugin functionality:
    - Added running clang-tidy directly from VS and displaying results in
    output window
    - Added highlighting places where validation warnings/errors were found
    plus intelisense quick info description (MEF component)
    - Added VS toolbar in addition to 'Tools' button
    - Added new icon for buttons
    - Modified project and manifest to reflect all changes and improve
    debugging capabilities

---
+++ clang-tidy-vs/ClangTidy/ClassifierAndTagger/QuickInfoSourceProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+
+namespace LLVM.ClangTidy
+{
+    [Export(typeof(IQuickInfoSourceProvider))]
+    [ContentType("code")]
+    [Name("ClangTidy QuickInfo Source")]
+    //[Order(Before = "Default Quick Info Presenter")]
+    internal class TestQuickInfoSourceProvider : IQuickInfoSourceProvider
+    {
+        [Import]
+        IBufferTagAggregatorFactoryService AggService = null;
+
+        public IQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer)
+        {
+            return new TestQuickInfoSource(textBuffer, AggService.CreateTagAggregator<ValidationTag>(textBuffer));
+        }
+    }
+}
